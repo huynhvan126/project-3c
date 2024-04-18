04636eb38f24a40ee5a6816ee6e2b5d8142f6e8d
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-04-18 00:03:38 -0700

    Add files via upload

---
+++ num_guess.py
@@ -0,0 +1,20 @@
+# Author: Van Huynh
+# GitHub username: huynhvan126
+# Date: 04/17/2024
+# Description: try to guess for an integer
+import random
+def num_guess():
+        target = int(input("Enter the integer for the player to guess. \n"))
+        guess = None
+        tries = 0
+        print("Enter your guess.")
+        while guess != target:
+            guess = int(input())
+            tries += 1
+            if guess > target:
+                print("Too high - try again:")
+            elif guess < target:
+                print("Too low - try again:")
+            else:
+                print(f"You guessed it in {tries} tries.")
+num_guess()
